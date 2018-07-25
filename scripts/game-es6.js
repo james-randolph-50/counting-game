e887803afefceb4be3e11994e92cb9e0555f4f1c
James <james.randolph2016@gmail.com>
2018-07-24 20:51:33 -0400

    defined canvas and stage, the latter of which is required by createjs. addded a ticker for Frames per Second

--- scripts/game-es6.js
+++ scripts/game-es6.js
@@ -1,6 +1,12 @@
 class Game {
     constructor() {
         console.log(`Welcome to the game. Version ${this.version()}`);
+    
+        this.canvas = document.getElementById("game-canvas");
+        this.stage = new createjs.Stage(this.canvas);
+
+        createjs.Ticker.setFPS(60);
+    
     }
         version(){
             return '1.0.0';
