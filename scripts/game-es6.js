30f03885ef89c8564e85abc2588ca8b0fd31b9ad
James <james.randolph2016@gmail.com>
2018-07-24 20:47:44 -0400

    changed js to backticks

--- scripts/game-es6.js
+++ scripts/game-es6.js
@@ -1,6 +1,6 @@
 class Game {
     constructor() {
-        console.log('Welcome to the game. Version ${this.version()}');
+        console.log(`Welcome to the game. Version ${this.version()}`);
     }
         version(){
             return '1.0.0';
