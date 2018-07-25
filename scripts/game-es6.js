69a91c463b7ca6e12bf11bf8abff2a98db8a6a9f
James <james.randolph2016@gmail.com>
2018-07-24 21:06:39 -0400

    added graphic files which have been converted to js.

--- scripts/game-es6.js
+++ scripts/game-es6.js
@@ -6,11 +6,13 @@ class Game {
         this.stage = new createjs.Stage(this.canvas);
 
         createjs.Ticker.setFPS(60);
+        createjs.Ticker.on("tick", this.stage);
     
     }
         version(){
             return '1.0.0';
         }
     }
-
+    
+//starts game
     var game = new Game();
